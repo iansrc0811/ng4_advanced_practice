b516bc366ad8333224f08a66df7c07ab3c852ea4
ian yeh <iansrc0811@hotmail.com>
2017-06-11 11:04:45 +0800

    預設路由, 轉向路由, 萬用路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
+  { path: '', component: Page1Component }, // 預設路由
   { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
+  { path: 'page2', component: Page2Component },
+  { path: 'page3', redirectTo: 'page1', pathMatch: 'full' }, // 轉向路由
+  { path: '**', redirectTo: 'page1', pathMatch: 'full'} // 萬用路由
 ];
 
 @NgModule({
