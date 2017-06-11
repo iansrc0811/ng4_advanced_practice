02ce852c5fae6e2915c95e28ef92cf716e4003b1
ian yeh <iansrc0811@hotmail.com>
2017-06-11 12:11:32 +0800

    加入 dashboard 和 cards  component，並套用路由.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import {Route, Routes,  RouterModule} from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
 
 const fallbackRoute: Route = {
   path: '**',
-  redirectTo: 'page1',
+  redirectTo: 'dashboard',
   pathMatch: 'full'
 } // 萬用路由}
 
 const routes: Routes = [
-  { path: '', component: Page1Component }, // 預設路由
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
-  { path: 'page3', redirectTo: 'page1', pathMatch: 'full' }, // 轉向路由
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cards', component: CardsComponent },
   fallbackRoute
 ];
 
