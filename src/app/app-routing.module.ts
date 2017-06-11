e89a96dbd7958cdca27d68cd774cc420a4700679
ian yeh <iansrc0811@hotmail.com>
2017-06-11 10:26:03 +0800

    routerLink 的寫法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true,
+    // useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
