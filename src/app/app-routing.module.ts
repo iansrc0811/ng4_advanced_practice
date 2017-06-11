e88a82525eff64f86f3cf1d7c2070e8c023bbd4b
ian yeh <iansrc0811@hotmail.com>
2017-06-11 11:16:33 +0800

    抽出路由變數

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Route, Routes,  RouterModule} from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+
+const fallbackRoute: Route = {
+  path: '**',
+  redirectTo: 'page1',
+  pathMatch: 'full'
+} // 萬用路由}
 
 const routes: Routes = [
   { path: '', component: Page1Component }, // 預設路由
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   { path: 'page3', redirectTo: 'page1', pathMatch: 'full' }, // 轉向路由
-  { path: '**', redirectTo: 'page1', pathMatch: 'full'} // 萬用路由
+  fallbackRoute
 ];
 
 @NgModule({
